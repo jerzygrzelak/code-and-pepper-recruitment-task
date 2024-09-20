f6991e54c63f09123beef02f0bdc4e76bfc93332
jerzygrzelak <greenbanzai@gmail.com>
2024-09-20 11:35:15 +0200

    improved typings in the service

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Person, Starship, SwapiGetResponse } from '../models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class SwapiService {
   constructor(private http: HttpClient) {
   }
 
-  getPeople(pageNumber: number = 1) {
+  getPeople(pageNumber: number = 1): Observable<SwapiGetResponse<Person>> {
     return this.http.get<SwapiGetResponse<Person>>(`https://swapi.dev/api/people/?page=${pageNumber}`);
   }
 
-  getStarships(pageNumber: number = 1) {
+  getStarships(pageNumber: number = 1): Observable<SwapiGetResponse<Starship>> {
     return this.http.get<SwapiGetResponse<Starship>>(`https://swapi.dev/api/starships/?page=${pageNumber}`);
   }
 }
